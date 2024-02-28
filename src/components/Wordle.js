50a15331dc19ba889dd2517e3d730cc1e873a845
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2024-02-28 07:28:45 +0430

    Add component folder

---
+++ src/components/Wordle.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import useWordle from "../hooks/useWordle";
+
+export default function Wordle({solution}) {
+    const { currentGuess, handleKeyUp } = useWordle(solution)
+    useEffect(() => {
+        window.addEventListener('keyup', handleKeyUp)
+        return () => window.removeEventListener('keyup', handleKeyUp)
+    }, [handleKeyUp])
+    return (
+        <div>Wordle</div>
+    )
+}
