ee27bdfcbe740f034770c99661691cabce1a9d0c
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2024-03-02 19:39:06 +0430

    Update usewordle for adding handlekeyup structure

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,19 +4,34 @@ const useWordle = (solution) => {
     const [turn, setTurn] = useState(0);
     const [currentGuess, setCurrentGuess] = useState('');
     const [guesses, setGuesses] = useState([]);
-    const [history, setHistory] = useState([]);
+    const [history, setHistory] = useState(['hello','ninja']);
     const [isCorrect, setIsCorrect] = useState(false);
 
     const formatGuess = () => {
-
+        console.log('formatting the guess - ', currentGuess)
     }
     const addNewGuess = () => {
 
     }
     const handleKeyUp = ({ key }) => {
-        if(key==='Backspace'){
-            setCurrentGuess((prev)=>{
-                return prev.slice(0,-1)
+        if (key === 'Enter') {
+            if (turn > 5) {
+                console.log('you used all your guesses')
+                return
+            }
+            if (history.includes(currentGuess)) {
+                console.log('you already tried that word')
+                return
+            }
+            if (currentGuess.length !== 5) {
+                console.log('word must be 5 chars long')
+                return
+            }
+            formatGuess()
+        }
+        if (key === 'Backspace') {
+            setCurrentGuess((prev) => {
+                return prev.slice(0, -1)
             })
             return
         }
