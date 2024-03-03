40932a86affb4864c5d0d4b2e17d45606b2151b4
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2024-03-03 19:59:27 +0430

    Update userwordle for adding utility to formatguess function

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,13 +1,16 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
 
-export default function Wordle({solution}) {
+export default function Wordle({ solution }) {
     const { currentGuess, handleKeyUp } = useWordle(solution)
     useEffect(() => {
         window.addEventListener('keyup', handleKeyUp)
         return () => window.removeEventListener('keyup', handleKeyUp)
     }, [handleKeyUp])
     return (
-        <div>current guess - {currentGuess}</div>
+        <div>
+            <div>solution - {solution}</div>
+            <div>current guess - {currentGuess}</div>
+        </div>
     )
 }
