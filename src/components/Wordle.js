7451243d089de5cd67930d943dfcc901c51332dc
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2024-03-07 07:17:47 +0430

    Update wordle for showing parameters

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -2,11 +2,14 @@ import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
 
 export default function Wordle({ solution }) {
-    const { currentGuess, handleKeyUp } = useWordle(solution)
+    const { currentGuess, handleKeyUp, guesses, isCorrect, turn } = useWordle(solution)
     useEffect(() => {
         window.addEventListener('keyup', handleKeyUp)
         return () => window.removeEventListener('keyup', handleKeyUp)
     }, [handleKeyUp])
+    useEffect(() => {
+        console.log(guesses, turn, isCorrect)
+    }, [guesses, turn, isCorrect])
     return (
         <div>
             <div>solution - {solution}</div>
