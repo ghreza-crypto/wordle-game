8bc2dcea9e5ddbfa1326b75056f75aef8c93091c
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2024-03-10 08:25:39 +0430

    Update wordle for adding grid and row component

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
+import Grid from "./Grid";
 
 export default function Wordle({ solution }) {
     const { currentGuess, handleKeyUp, guesses, isCorrect, turn } = useWordle(solution)
@@ -14,6 +15,7 @@ export default function Wordle({ solution }) {
         <div>
             <div>solution - {solution}</div>
             <div>current guess - {currentGuess}</div>
+            <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
         </div>
     )
 }
