970294e6624831e7af0be59929d062ee0b674327
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2024-03-14 09:50:29 +0430

    Update Grid for adding guess prop

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,7 +4,7 @@ export default function Grid({ currentGuess, guesses, turn }) {
     return (
         <div>
             {guesses.map((g, i) => {
-                return <Row key={i} />
+                return <Row key={i} guess={g}/>
             })}
         </div>
     )
