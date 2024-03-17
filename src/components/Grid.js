5371f76577001d51d9723659e41b8eb3788ff260
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2024-03-17 09:53:05 +0430

    Update Grid for adding current guess row

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,7 +4,10 @@ export default function Grid({ currentGuess, guesses, turn }) {
     return (
         <div>
             {guesses.map((g, i) => {
-                return <Row key={i} guess={g}/>
+                if (turn === i) {
+                    return <Row key={i} currentGuess={currentGuess} />
+                }
+                return <Row key={i} guess={g} />
             })}
         </div>
     )
