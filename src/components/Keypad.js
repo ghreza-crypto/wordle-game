77f14e5bb448e2b0c207d8ce9b52531cd38b0050
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2024-03-22 07:28:05 +0430

    Create Keypad for showing letters

---
+++ src/components/Keypad.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+export default function Keypad() {
+    const [letters, setLetters] = useState([
+        { "key": "a" }, { "key": "b" }, { "key": "c" }, { "key": "d" }, { "key": "e" }, { "key": "f" }, { "key": "g" }, { "key": "h" }, { "key": "i" }, { "key": "j" }, { "key": "k" }, { "key": "l" }, { "key": "m" }, { "key": "n" }, { "key": "o" }, { "key": "p" }, { "key": "q" }, { "key": "r" }, { "key": "s" }, { "key": "t" }, { "key": "u" }, { "key": "v" }, { "key": "w" }, { "key": "x" }, { "key": "y" }, { "key": "z" }
+    ])
+    return (
+        <div className="keypad">
+            {letters.map((l) => {
+                return (
+                    <div key={l.key}>{l.key}</div>
+                )
+            })}
+        </div>
+    )
+}
