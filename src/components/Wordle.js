ea0a070f10ae3368a6f09ec645f395e435c1d783
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2024-03-23 07:28:51 +0430

    Update wordle for showing keypad in wordle

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
+import Keypad from "./Keypad";
 
 export default function Wordle({ solution }) {
     const { currentGuess, handleKeyUp, guesses, isCorrect, turn } = useWordle(solution)
@@ -16,6 +17,7 @@ export default function Wordle({ solution }) {
             <div>solution - {solution}</div>
             <div>current guess - {currentGuess}</div>
             <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+            <Keypad />
         </div>
     )
 }
