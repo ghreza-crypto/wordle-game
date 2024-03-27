f1fde6a70dbbc801196c8905469916a4973cf346
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2024-03-27 07:58:31 +0430

    Update keypad for adding class for colors

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
-export default function Keypad() {
+export default function Keypad({ usedKeys }) {
     const [letters, setLetters] = useState([
         { "key": "a" }, { "key": "b" }, { "key": "c" }, { "key": "d" }, { "key": "e" }, { "key": "f" }, { "key": "g" }, { "key": "h" }, { "key": "i" }, { "key": "j" }, { "key": "k" }, { "key": "l" }, { "key": "m" }, { "key": "n" }, { "key": "o" }, { "key": "p" }, { "key": "q" }, { "key": "r" }, { "key": "s" }, { "key": "t" }, { "key": "u" }, { "key": "v" }, { "key": "w" }, { "key": "x" }, { "key": "y" }, { "key": "z" }
     ])
     return (
         <div className="keypad">
             {letters.map((l) => {
+                const color = usedKeys[l.key]
                 return (
-                    <div key={l.key}>{l.key}</div>
+                    <div key={l.key} className={color}>{l.key}</div>
                 )
             })}
         </div>
